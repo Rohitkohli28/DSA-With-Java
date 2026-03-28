dbf6a8a99a20c28485fcd4d3e9fc4459b564c49d
Rohit Kohli <kohlirohit2428@gmail.com>
2026-03-28 12:15:22 +0530

    More Questions

---
+++ Greedy Algrorithm/src/JumpGameII.java
@@ -0,0 +1,30 @@
+public class JumpGameII {
+
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+
+                if (currentEnd >= nums.length - 1) {
+                    break;
+                }
+            }
+        }
+
+        return jumps;
+    }
+
+    public static void main(String[] args) {
+        JumpGameII solution = new JumpGameII();
+        int[] nums = {2, 3, 1, 1, 4};
+        int result = solution.jump(nums);
+        System.out.println("Minimum number of jumps: " + result); // Output: 2
+    }
+}
